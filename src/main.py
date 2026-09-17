a83a3cdc9a2f05665387e96d3ef218145e3a319e
FreshGame <vasin.oleg2010@mail.ru>
2026-09-17 12:43:01 +1000

    rm

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from ui_mainwindow import FLauncher
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = FLauncher()
-    window.show()
-    sys.exit(app.exec())
